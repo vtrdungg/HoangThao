eaca747aa61526ba9abdea0207e4f28672b7f949
vtrdungg <135849373+vtrdungg@users.noreply.github.com>
2025-06-12 18:59:35 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `I have something for you`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
