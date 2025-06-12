b5bd41508ab406e55eb95bd250a0aabf1d5582af
vtrdungg <135849373+vtrdungg@users.noreply.github.com>
2025-06-12 19:03:52 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY BIRTHDAY HOANG THAO').split('')
+    const titles = ('HAPPY BIRTHDAY HOÀNG THẢO').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
